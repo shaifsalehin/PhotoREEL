df285223ae807e363beb342d9a6d71322db54e86
Shaif Salehin <github@shaifsalehin.com>
2022-10-20 22:46:58 -0500

    Initial commit

---
+++ src/main/java/com/shaifsalehin/photoreel/PhotoReelApplication.java
@@ -0,0 +1,13 @@
+package com.shaifsalehin.photoreel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoReelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoReelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shaifsalehin/photoreel/PhotoReelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaifsalehin.photoreel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoReelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
